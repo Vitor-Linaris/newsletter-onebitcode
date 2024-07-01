12ab714c3659b038681d80f00510d878014a50cd
Vitor-Linaris <linaris@DESKTOP-D7AD2JN>
2024-07-01 14:08:05 -0300

    feat: complete challenge

--- src/server.js
+++ src/server.js
@@ -1,34 +1,47 @@
-const express = require('express');
-const path = require('node:path');
+const express = require("express");
+const path = require("node:path");
 
 const app = express();
 
-storedEmail = [];
+let storedEmail = [];
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/register', (req, res) => {
-    const email = req.body.email
-    storedEmail.push({email})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-    res.redirect('/success')
-} )
+app.post("/register", (req, res) => {
+  const { email } = req.body;
 
-app.get('/success', (req, res) => {
-    res.render('success')
-})
-app.get('/list', (req, res) => {
-    res.render('listEmails', {emails: storedEmail})
-})
+  if (email) {
+    storedEmail.push(email);
+    res.redirect("/success");
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.get("/success", (req, res) => {
+  res.render("success");
+});
+
+app.get("/list", (req, res) => {
+  res.render("listEmails", { emails: storedEmail });
+});
+
+app.post("/list/delete", (req, res) => {
+  const { email } = req.body;
+  storedEmail = storedEmail.filter((item) => item !== email);
+  res.redirect("/list");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`servidor rodando na porta ${PORT}`)
-})
+  console.log(`servidor rodando na porta ${PORT}`);
+});
